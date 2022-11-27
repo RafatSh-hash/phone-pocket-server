61b74f587988f030b28acdcfd392f84eba18d063
Rafatsh-hash <rafatsh800@gmail.com>
2022-11-28 03:01:11 +0600

    Seller Verification Done

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ const { query } = require("express");
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send("Unauthorized Access");
-  }
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: "forbidden access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5crvfi4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
